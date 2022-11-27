893e6b8d903e818da5e59afda3fab3d32bab9c68
edewindt <edewindt27@outlook.com>
2022-11-27 03:42:04 -0600

    Added date formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,25 @@
 <script>
-	let expenses = [{ name: 'Food', cost: 6.99, negative: true }];
+	const now = new Date();
+
+	let expenses = [
+		{
+			name: 'Food',
+			cost: 6.99,
+			negative: true,
+			time: dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT')
+		}
+	];
+	import dateFormat, { masks } from 'dateformat';
 </script>
 
 <h2>Expense Tracker</h2>
 
 <table>
-	{#each expenses as { name, cost, negative }}
-		<tr><td>{name}</td><td>{cost}</td><td>{negative}</td></tr>
+	<tr><th>Expense</th><th>Value</th></tr>
+	{#each expenses as { name, cost, negative, time }}
+		<tr><td>{name}</td><td>{cost}</td><td>{time}</td></tr>
 	{/each}
 </table>
+
+<style>
+</style>
